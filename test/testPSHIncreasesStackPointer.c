da7ac27da4b6ea28f6dc3a1eca7d211e0a0a4694
Hogoww <pierre_misse25@msn.com>
2023-01-10 15:21:33 +0100

    more stack test cases

---
+++ test/testPSHIncreasesStackPointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../src/cgafc.h"
+#include "cAssert.h"
+#include "instructionBuildingTool.h"
+
+int main(void){
+  ObservableVM ovm;
+  initializeObservableVM(&ovm);
+  
+  addInstruction(&ovm,liw(A_REGISTER));
+  addWord(&ovm,0xDEAD);
+  addInstruction(&ovm,psh(A_REGISTER));
+  addInstruction(&ovm,halt());
+  executeUntilHalt(&ovm);
+
+  assert(ovm.vm.sX.p==1);
+  return 0;
+}
